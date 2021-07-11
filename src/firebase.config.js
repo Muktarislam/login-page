fc28f4f873ce47bbdc235bcadcb83d13ce818579
Muktar Islam <iammuktarislam@.com>
2021-07-12 02:23:37 +0600

    done

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD15sQlwRL8FhXpJCtfEhhOW9rZyZEInGE",
+  authDomain: "practice-3feef.firebaseapp.com",
+  projectId: "practice-3feef",
+  storageBucket: "practice-3feef.appspot.com",
+  messagingSenderId: "671064214410",
+  appId: "1:671064214410:web:07340fd36301cc08b35673"
+};
+  export default firebaseConfig;
